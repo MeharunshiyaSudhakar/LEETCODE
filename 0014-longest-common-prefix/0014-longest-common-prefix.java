4eb6d1cbee31b3f0fb18f07100aec027de30b446
Meharunshiya S <meharunshiyas.23csd@kongu.edu>
2026-06-11 21:07:57 +0530

    Time: 1 ms (62.63%), Space: 43.4 MB (32.27%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs==null || strs.length==0){
+            return "";
+        }
+        String s=strs[0];
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            for(int j=1;j<strs.length;j++){
+                if(i>=strs[j].length() || ch!=strs[j].charAt(i)){
+                    return s.substring(0,i);
+                }
+            }
+        }
+        return s;
+    }
+}
